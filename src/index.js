b296566130db6559264bda7e7423f2feab64a782
Vinod Chauhan <vinodc45@gmail.com>
2019-12-26 13:24:38 +0530

    Add Loader to handle css Install style-loader and css-loader configured webpack.config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { HandleError } from "./handleError";
 import { Service } from "./service";
 import { main } from "./app";
+import "./index.css";
 
 const handleError = new HandleError();
 const service = new Service();
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
