194985095f6e24628a8c69e05f2d586bea96634c
Vinod Chauhan <vinodc45@gmail.com>
2019-12-26 12:45:55 +0530

    Add webpack config file Created webpack.config.js Added some basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  devtool: "none",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  }
+};
